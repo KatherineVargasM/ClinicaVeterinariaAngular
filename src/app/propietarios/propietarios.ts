ba69c092e437959c2e074499d45968da41ae4e05
KatherineVargasM <katherinevargas15kd@gmail.com>
2026-06-28 01:19:25 -0500

    Commit2-ImprimirPDF

--- src/app/propietarios/propietarios.ts
+++ src/app/propietarios/propietarios.ts
@@ -3,6 +3,11 @@ import { PropietariosService } from '../services/propietarios.service';
 import { Propietario } from '../interfaces/propietario';
 import { RouterLink } from '@angular/router';
 
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(pdfMake as any).vfs = (pdfFonts as any).vfs;
+
 @Component({
   selector: 'app-propietarios',
   standalone: true,
@@ -24,11 +29,56 @@ export class Propietarios implements OnInit {
       next: (propietarios) => {
         this.lista = propietarios;
         this.cdr.detectChanges();
-        console.log('Lista cargada:', this.lista);
       },
       error: (err) => {
         console.error('Error al cargar propietarios:', err);
       }
     });
   }
+  imprimirPDF() {
+  const cuerpoTabla = [
+    ['ID', 'Nombre', 'Teléfono', 'Email', 'Dirección'],
+    ...this.lista.map(p => [
+      p.id,
+      p.nombre,
+      p.telefono,
+      p.correo,
+      p.direccion
+    ])
+  ];
+
+  const documento: any = {
+  pageOrientation: 'landscape',
+
+  content: [
+    {
+      text: 'REPORTE DE PROPIETARIOS',
+      style: 'titulo'
+    },
+    {
+      table: {
+        headerRows: 1,
+        widths: [35, 130, 90, 190, '*'],
+        body: cuerpoTabla
+      },
+      layout: 'lightHorizontalLines'
+    }
+  ],
+
+  styles: {
+    titulo: {
+      fontSize: 18,
+      bold: true,
+      alignment: 'center',
+      margin: [0,0,0,15]
+    }
+  },
+
+  defaultStyle: {
+    fontSize: 10
+  }
+};
+
+  pdfMake.createPdf(documento).open();
+}
 }
